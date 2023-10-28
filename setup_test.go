0cdd379090d3b3b8beacb4a2d9f868ac8536a694
pi <pi@ovya.fr>
2023-10-28 14:41:38 +0200

    Remove zerolog dependency

--- setup_test.go
+++ setup_test.go
@@ -3,18 +3,10 @@ package nullable
 import (
 	"os"
 	"testing"
-
-	"github.com/rs/zerolog"
-	"github.com/stretchr/testify/assert"
 )
 
 // Setup
 func TestMain(m *testing.M) {
-	zerolog.SetGlobalLevel(zerolog.Disabled)
 	code := m.Run()
 	os.Exit(code)
-}
-
-func TestEmpty(t *testing.T) {
-	assert.True(t, true, "C'est pas faux !")
 }
